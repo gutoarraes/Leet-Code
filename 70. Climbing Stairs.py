62722321dfeae426b07c3fca99a96352fda6f13e
Augusto Arraes <augustobarraes@gmail.com>
2021-11-01 00:22:46 -0400

    finished exercise 70

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -4,12 +4,20 @@
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        count = 0
-        while n > =0:
-            if n % 2 == 0:
-                n -= 2
-                count += 2
-            elif n == 1:
-                count += 1
-                n -= 1
-        return count
+        # while n > 0:
+        #     if n >= 2:
+        #         n -= 2
+        #         count += 2
+        #     elif n == 1:
+        #         count += 1
+        #         n -= 1
+        # print(count)
+        # return count
+        a = b = 1
+        for i in range(n):
+            a, b = b, a+b
+        return b
+
+a = Solution()
+a.climbStairs(4)
+
